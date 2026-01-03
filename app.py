c56d440f993abf31e4f17966daed919a30ae4802
systemadmin-lab <rejuananik@gmail.com>
2026-01-03 08:36:15 -0800

    folder structure done

---
+++ app.py
@@ -0,0 +1,7 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+#from streamlit as st
+import streamlit as st
+import os
+from dotenv import load_dotenv
