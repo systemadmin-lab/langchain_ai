427b9f5a13414865e78532d09ebef3e3acb44ca1
systemadmin-lab <rejuananik@gmail.com>
2026-01-05 00:09:01 -0800

    langchain simple bot ready

--- app.py
+++ app.py
@@ -9,4 +9,20 @@ from dotenv import load_dotenv
 os.environ["OPENAI_API_KEY"] =os.getenv("OPENAI_API_KEY")
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRCING_V2"] = "true"
+## prompt template
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant that helps people find information."),
+    ("user","Question:{question}")
+    
+])
+## streamlit framework
+st.title("Langchain with OpenAI Chat Model")
+inpput_question = st.text_input("Enter your question here")
 
+# llm model
+llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+output_parser = StrOutputParser()
+chain = prompt | llm | output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":inpput_question}))
