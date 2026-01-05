6dd8d3d1e23268d72b8db5b2bc49af6fd8f491df
systemadmin-lab <rejuananik@gmail.com>
2026-01-04 23:15:08 -0800

    api key code added

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
 import os
 from dotenv import load_dotenv
+
+os.environ["OPENAI_API_KEY"] =os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRCING_V2"] = "true"
+
